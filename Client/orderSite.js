f1e8abc17cba71d40f13798197095f9e25355590
Unknown <mikk633k@hotmail.com>
2018-06-13 22:09:30 +0200

    if statuscode 401 unauthorized occurs redirect to login page

--- Client/orderSite.js
+++ Client/orderSite.js
@@ -1,7 +1,21 @@
-
-let loginRes = JSON.parse(new URLSearchParams(window.location.search).get('data'))
-$.ajaxSetup({ headers: { 'Authorization': 'Basic ' + loginRes.Token }});
 let server = 'https://ticket-api.nas-tech.dk/API/'
+let loginRes = undefined
+const onload = () => {
+    var uri = window.location.toString();
+    if (uri.indexOf("?") > 0) {
+        loginRes = JSON.parse(new URLSearchParams(window.location.search).get('data'))
+        $.ajaxSetup({ headers: { 'Authorization': 'Basic ' + loginRes.Token }});
+
+        var clean_uri = uri.substring(0, uri.indexOf("?"));
+        window.history.replaceState({}, document.title, clean_uri);
+    }
+}
+
+$(document).ajaxError((event, jqxhr, settings, exception) => {
+    if (jqxhr.status === 401) {
+        document.location.href = 'index.html'
+    }
+});
 
 function orderCreate() {
     // set the header to contain the token to all future requests
