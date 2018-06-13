c500ff3d7492894346dfbe5eeb07f4c8cfe79474
Unknown <mikk633k@hotmail.com>
2018-06-13 13:16:31 +0200

    login

--- Client/login.js
+++ Client/login.js
@@ -1,9 +1,8 @@
 function login() {
-    console.log('called...')
     $.ajax({
         url: 'https://ticket-api.nas-tech.dk/API/User/Login',
         type: 'POST',
-        data: "{'UserName': 'Mikkel', 'Password': '123456'}",
+        data: json.serialize({UserName: 'Mikkel', Password: '123456'}),
         dataType: 'json',
         contentType: 'application/json',
         success: (res) => {
