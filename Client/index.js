bb6a6c3c2b212e68ee70361fe8759ef9d7c82cad
Unknown <mikk633k@hotmail.com>
2018-06-14 13:38:25 +0200

    Auto stash before merge of "ClientDev" and "origin/ClientDev"

--- Client/index.js
+++ Client/index.js
@@ -11,21 +11,3 @@ const showSignin = () => {
         $("#login").slideDown("slow");
     });
 }
-	function Load(url) {
-    // display loading image here...
-    document.getElementById('loading').visible = true;
-    // request your data...
-    var req = new XMLHttpRequest();
-    req.open("POST", url, true);
-
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            // content is loaded...hide the gif and display the content...
-            if (req.responseText) {
-                document.getElementById('content').innerHTML = req.responseText;
-                document.getElementById('loading').visible = false;
-            }
-        }
-    }
-    request.send(vars);
-}
